7d9cc7e939fdcaddb95bc24000728449dfb91e99
Ivor <ikojun00@fesb.hr>
2024-07-24 17:05:54 +0200

    designing playground

--- app/page.tsx
+++ app/page.tsx
@@ -132,92 +132,154 @@ export default function Home() {
   }, [handleCardSelect]);
 
   return (
-    <div>
-      {players.map((player, playerIndex) => (
-        <div key={player.id}>
-          <h2
-            className={
-              playerIndex === currentTurn ? "font-bold" : "text-slate-200"
-            }
-          >
-            {player.id}
-          </h2>
-
-          <div className="flex">
-            {player.hand.map((card, cardIndex) =>
-              socket.id === player.id ? (
-                <button
-                  key={cardIndex}
-                  className="w-20 h-40 p-1 rounded-md border-black border-2 relative hover:transform hover:-translate-y-4"
-                  onClick={() => {
-                    socket.emit("select_card", playerIndex, cardIndex);
-                  }}
+    <div className="flex flex-col min-h-screen">
+      <div className="flex flex-col items-center">
+        {/* Opponents' Cards at the Top */}
+        <div className="flex gap-20">
+          {players
+            .filter((player) => player.id !== socket.id)
+            .map((player) => (
+              <div key={player.id} className="mb-4">
+                <h2
+                  className={
+                    players.findIndex((e) => e.id !== socket.id) === currentTurn
+                      ? "font-bold"
+                      : "text-slate-200"
+                  }
                 >
-                  <div className="h-full flex flex-col justify-between items-center rounded-md border-black border-2">
-                    <p className="text-xs self-start p-0.5">{card.value}</p>
-                    <p className="flex items-center justify-center">
-                      {card.suit}
-                    </p>
-                    <p className="text-xs self-end p-0.5">{card.value}</p>
-                  </div>
-                </button>
-              ) : (
-                <div
-                  key={cardIndex}
-                  className="w-20 rounded-md border-2 border-black"
-                >
-                  <Image
-                    src="/back.webp"
-                    width={0}
-                    height={0}
-                    sizes="100vw"
-                    style={{ width: "100%", height: "auto" }}
-                    alt="Back of card"
-                  />
+                  {player.id}
+                </h2>
+                <div className="flex">
+                  {player.hand.map((card, cardIndex) => (
+                    <div
+                      key={cardIndex}
+                      className="w-20 rounded-md border-2 border-black"
+                    >
+                      <Image
+                        src="/back.webp"
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{ width: "100%", height: "auto" }}
+                        alt="Back of card"
+                      />
+                    </div>
+                  ))}
                 </div>
-              )
-            )}
+              </div>
+            ))}
+          <div className="w-20 mt-6 relative mb-4">
+            <Image
+              src="/back.webp"
+              width={0}
+              height={0}
+              className="absolute inset-0 w-full h-auto border-2 border-black rounded-md"
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+              alt="Back of card"
+            />
+            <p className="absolute inset-0 flex items-center justify-center">
+              <span className="bg-white text-black px-2 py-1 rounded-md">
+                {currentPoints[players.findIndex((e) => e.id !== socket.id)]}
+              </span>
+            </p>
           </div>
-          {selectedCards[playerIndex] && (
-            <div className="w-20 h-40 p-1 rounded-md border-black border-2">
+        </div>
+
+        {/* Selected Cards in the Middle */}
+        <div className="flex items-center justify-center mt-auto mb-4">
+          {selectedCards.map((card, index) => (
+            <div
+              key={index}
+              className="w-20 h-40 p-1 rounded-md border-black border-2 m-2"
+            >
               <div className="h-full flex flex-col justify-between items-center rounded-md border-black border-2">
-                <p className="text-xs self-start p-0.5">
-                  {selectedCards[playerIndex].value}
-                </p>
-                <p className="flex items-center justify-center">
-                  {selectedCards[playerIndex].suit}
-                </p>
-                <p className="text-xs self-end p-0.5">
-                  {selectedCards[playerIndex].value}
-                </p>
+                <p className="text-xs self-start p-0.5">{card.value}</p>
+                <p className="flex items-center justify-center">{card.suit}</p>
+                <p className="text-xs self-end p-0.5">{card.value}</p>
               </div>
             </div>
+          ))}
+
+          {briscola ? (
+            <div className="w-20 h-40 p-1 rounded-md border-black border-2 ml-4">
+              <div className="h-full flex flex-col justify-between items-center rounded-md border-black border-2">
+                <p className="text-xs self-start p-0.5">{briscola.value}</p>
+                <p className="flex items-center justify-center">
+                  {briscola.suit}
+                </p>
+                <p className="text-xs self-end p-0.5">{briscola.value}</p>
+              </div>
+            </div>
+          ) : (
+            "No Briscola"
           )}
         </div>
-      ))}
-      {briscola ? (
-        <div className="w-20 h-40 p-1 rounded-md border-black border-2">
-          <div className="h-full flex flex-col justify-between items-center rounded-md border-black border-2">
-            <p className="text-xs self-start p-0.5">{briscola.value}</p>
-            <p className="flex items-center justify-center">{briscola.suit}</p>
-            <p className="text-xs self-end p-0.5">{briscola.value}</p>
+
+        {/* Current Player's Cards at the Bottom */}
+        <div className="flex gap-20 items-center justify-end">
+          {players
+            .filter((player) => player.id === socket.id)
+            .map((player) => (
+              <div key={player.id} className="mt-4">
+                <h2
+                  className={
+                    players.findIndex((e) => e.id === socket.id) === currentTurn
+                      ? "font-bold"
+                      : "text-slate-200"
+                  }
+                >
+                  {player.id}
+                </h2>
+                {player.hand.map((card, cardIndex) => (
+                  <button
+                    key={cardIndex}
+                    className="w-20 h-40 p-1 rounded-md border-black border-2 relative hover:transform hover:-translate-y-4"
+                    onClick={() => {
+                      socket.emit(
+                        "select_card",
+                        players.findIndex((e) => e.id === socket.id),
+                        cardIndex
+                      );
+                    }}
+                  >
+                    <div className="h-full flex flex-col justify-between items-center rounded-md border-black border-2">
+                      <p className="text-xs self-start p-0.5">{card.value}</p>
+                      <p className="flex items-center justify-center">
+                        {card.suit}
+                      </p>
+                      <p className="text-xs self-end p-0.5">{card.value}</p>
+                    </div>
+                  </button>
+                ))}
+              </div>
+            ))}
+          <div className="w-20 relative">
+            <Image
+              src="/back.webp"
+              width={0}
+              height={0}
+              className="absolute inset-0 w-full h-auto border-2 border-black rounded-md"
+              sizes="100vw"
+              style={{ width: "100%", height: "auto" }}
+              alt="Back of card"
+            />
+            <p className="absolute inset-0 flex items-center justify-center">
+              <span className="bg-white text-black px-2 py-1 rounded-md">
+                {currentPoints[players.findIndex((e) => e.id === socket.id)]}
+              </span>
+            </p>
           </div>
         </div>
-      ) : (
-        "No Briscola"
-      )}
-      <h2>
-        Points:{" "}
-        {players.length > 0
-          ? `${players[0].id} ${currentPoints[0]} - ${players[1].id} ${currentPoints[1]}`
-          : "N/A"}
-      </h2>
-      <br />
-      {cards.map((card, index) => (
-        <p key={index}>
-          {card.suit} {card.value}
-        </p>
-      ))}
+      </div>
+      <div className="flex justify-center">
+        <h2>
+          Points:{" "}
+          {players.length > 0
+            ? `${players[0].id} ${currentPoints[0]} - ${players[1].id} ${currentPoints[1]}`
+            : "N/A"}
+        </h2>
+      </div>
       {currentPoints[0] > 60 && <p>{players[0].name} won!</p>}
       {currentPoints[1] > 60 && <p>{players[1].name} won!</p>}
     </div>
